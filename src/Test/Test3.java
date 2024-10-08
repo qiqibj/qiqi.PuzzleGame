2df11e0d2b26f2814f9d01f600045d43a2a0b455
qiqibj <168752861+qiqibj@users.noreply.github.com>
2024-10-08 15:51:24 +0800

    Delete src/Test directory

--- src/Test/Test3.java
+++
@@ -1,7 +0,0 @@
-package Test;
-
-public class Test3 {
-    public static void main(String[] args) {
-        new MyJFrame();
-    }
-}
--- src/Test/test4.java
+++
@@ -1,7 +0,0 @@
-package Test;
-
-public class test4 {
-    public static void main(String[] args) {
-        new MyJFrame3();
-    }
-}
